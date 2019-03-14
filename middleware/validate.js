124833dc3d61511d10dfbbd99a93aaf307d31378
donatnshimiyimana <[donatnshimiyimana@gmail.com]>
2019-03-14 14:32:28 +0200

    This is the The validation of user loging in, creating account or sending message

---
+++ middleware/validate.js
@@ -0,0 +1,44 @@
+import Joi from "joi";
+
+const userSchema = Joi.object().keys({
+  firstname: Joi.string().alphanum().min(5).max(15)
+    .required(),
+  lastname: Joi.string().alphanum().min(5).max(15)
+    .required(),
+  email: Joi.string().email({ minDomainAtomas: 2 })
+    .required(),
+  password: Joi.string().alphanum().min(3).max(15)
+    .required(),
+});
+
+const loginSchema = Joi.object().keys({
+  email: Joi.string().email({ minDomainAtomas: 2 })
+    .required(),
+  password: Joi.string().alphanum().min(3).max(15)
+    .required(),
+});
+
+const messageSchema = Joi.object().keys({
+  subject: Joi.string().trim().min(5).max(25)
+    .required(),
+  message: Joi.string().trim().min(10)
+    .max(100)
+    .required(),
+  senderId: Joi.number().integer()
+    .required(),
+  receiverId: Joi.number().integer()
+    .required(),
+  parentMessageId: Joi.number().integer()
+    .required(),
+  status: Joi.string().alphanum().valid("sent", "draft", "read")
+    .required(),
+});
+
+const emailParams = Joi.object().keys({
+  emailId: Joi.number().integer()
+    .required(),
+});
+
+export default {
+  userSchema, loginSchema, messageSchema, emailParams,
+};
